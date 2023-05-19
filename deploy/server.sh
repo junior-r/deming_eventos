e7dedd8bf18ec5757c1b6ea61c10258761f76a10
Junior Ruiz <junior31064049@gmail.com>
2023-05-19 15:12:16 -0500

    Change bash file for deploy

--- deploy/server.sh
+++ deploy/server.sh
@@ -2,6 +2,6 @@
 DJANGODIR=$(dirname $(cd `dirname $0` && pwd))
 DJANGO_SETTINGS_MODULE=eventos_deming.settings
 cd $DJANGODIR
-source env/local/bin/activate
+source env/bin/activate
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 exec python manage.py runserver 0:9000
